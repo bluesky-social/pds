2a31e98c86f2ba5ef2c864296fdda8e736732825
dholms <dtholmgren@gmail.com>
2024-02-15 20:41:25 -0600

    update service entry

--- service/index.js
+++ service/index.js
@@ -1,7 +1,6 @@
 "use strict";
 const {
   PDS,
-  Database,
   envToCfg,
   envToSecrets,
   readEnv,
@@ -15,19 +14,9 @@ const main = async () => {
   const cfg = envToCfg(env);
   const secrets = envToSecrets(env);
   const pds = await PDS.create(cfg, secrets);
-  if (cfg.db.dialect === "pg") {
-    // Migrate using credentialed user
-    const migrateDb = Database.postgres({
-      url: cfg.db.migrationUrl,
-      schema: cfg.db.schema,
-    });
-    await migrateDb.migrateToLatestOrThrow();
-    await migrateDb.close();
-  } else {
-    await pds.ctx.db.migrateToLatestOrThrow();
-  }
   await pds.start();
   httpLogger.info("pds is running");
+
   // Graceful shutdown (see also https://aws.amazon.com/blogs/containers/graceful-shutdowns-with-ecs/)
   process.on("SIGTERM", async () => {
     httpLogger.info("pds is stopping");
