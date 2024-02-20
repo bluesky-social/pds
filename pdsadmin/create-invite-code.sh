27c760491273ccf0fc579729c825a4c91640ee6a
Jake Gold <jake@blueskyweb.xyz>
2024-02-20 11:57:46 -0800

    pdsadmin: add create-invite-code

---
+++ pdsadmin/create-invite-code.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+PDS_ENV_FILE="/pds/pds.env"
+
+if [[ -f "${PDS_ENV_FILE}" ]]; then
+  source "${PDS_ENV_FILE}"
+fi
+
+curl --silent \
+  --show-error \
+  --request POST \
+  --user "admin:${PDS_ADMIN_PASSWORD}" \
+  --header "Content-Type: application/json" \
+  --data '{"useCount": 1}' \
+  https://${PDS_HOSTNAME}/xrpc/com.atproto.server.createInviteCode
