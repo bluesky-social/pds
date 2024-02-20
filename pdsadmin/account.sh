68a04a9bc0fcb988d31917be4f0944028742cd98
Jake Gold <jake@blueskyweb.xyz>
2024-02-20 11:44:07 -0800

    add pdsadmin helper script

---
+++ pdsadmin/account.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "TODO"
---
+++ pdsadmin/help.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# This script is used to display help information for the pdsadmin command.
+cat <<HELP
+pdsadmin help
+--
+update <VERSION>
+  Update to the specific PDS version.
+    e.g. pdsadmin update 0.1.1
+
+account
+  list
+    List accounts
+    e.g. pdsadmin account list
+  create <EMAIL> <HANDLE> <DOB>
+    Create a new account
+    e.g. pdsadmin account create alice@example.com alice.example.com 08/04/2004
+  delete <DID>
+    Delete an account specified by DID.
+    e.g. pdsadmin account takedown did:plc:xyz123abc456
+  takedown <DID>
+    Takedown an account specified by DID.
+    e.g. pdsadmin account takedown did:plc:xyz123abc456
+  untakedown <DID>
+    Remove a takedown an account specified by DID.
+    e.g. pdsadmin account takedown did:plc:xyz123abc456
+
+request-crawl <RELAY HOST>
+    Request a crawl from a relay host.
+    e.g. pdsadmin request-crawl bsky.network
+
+help
+    Display this help information.
+
+HELP
