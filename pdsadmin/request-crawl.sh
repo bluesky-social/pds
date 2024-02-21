35379cd3b20812dd7f6d76f516107fc4b57744f6
Jake Gold <jake@blueskyweb.xyz>
2024-02-21 08:06:02 -0800

    minor style changes to pdsadmin request-crawl

--- pdsadmin/request-crawl.sh
+++ pdsadmin/request-crawl.sh
@@ -6,17 +6,19 @@ set -o pipefail
 PDS_ENV_FILE="/pds/pds.env"
 source "${PDS_ENV_FILE}"
 
-RELAY_HOSTS="${1:-$PDS_CRAWLERS}"
+RELAY_HOSTS="${1:-}"
+if [[ "${RELAY_HOSTS}" == "" ]]; then
+  RELAY_HOSTS="${PDS_CRAWLERS}"
+fi
 
 if [[ "${RELAY_HOSTS}" == "" ]]; then
-  echo "Missing RELAY HOST parameter." >/dev/stderr
-  echo "Usage: $0 [<RELAY HOST>]" >/dev/stderr
+  echo "ERROR: missing RELAY HOST parameter." >/dev/stderr
+  echo "Usage: $0 <RELAY HOST>[,<RELAY HOST>,...]" >/dev/stderr
   exit 1
 fi
 
-for host in ${RELAY_HOSTS//,/ }
-do
-  echo "Requesting crawl from $host"
+for host in ${RELAY_HOSTS//,/ }; do
+  echo "Requesting crawl from ${host}"
   curl \
     --fail \
     --silent \
@@ -24,7 +26,7 @@ do
     --request POST \
     --user "admin:${PDS_ADMIN_PASSWORD}" \
     --header "Content-Type: application/json" \
-    --data "{\"hostname\": \"$PDS_HOSTNAME\"}" \
+    --data "{\"hostname\": \"${PDS_HOSTNAME}\"}" \
     https://${host}/xrpc/com.atproto.sync.requestCrawl >/dev/null
 done
 
